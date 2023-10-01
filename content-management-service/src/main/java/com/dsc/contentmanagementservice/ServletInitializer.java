3f7531131c920ab01acdf3bef9a777caa4ad6bb8
hua2980 <95843009+hua2980@users.noreply.github.com>
2023-10-01 10:52:54 +0800

    initial commit

---
+++ content-management-service/src/main/java/com/dsc/contentmanagementservice/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.dsc.contentmanagementservice;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(ContentManagementServiceApplication.class);
+  }
+
+}
---
+++ content-management-service/src/test/java/com/dsc/contentmanagementservice/ContentManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsc.contentmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentManagementServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
